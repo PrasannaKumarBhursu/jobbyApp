509882ba58cc66c0c3cd3b2efeff156901660640
Anjan <vinayanjaneyulu2@gmail.com>
2022-02-03 23:54:25 +0530

    changed all functional components into class components

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,44 +1,93 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import JobbyHeader from "../JobbyHeader";
+// import { Spinner } from "react-bootstrap";
+// import "./index.css";
+
+// export const NotFound = () => {
+//   const [showSpinner, setShowSpinner] = useState(false);
+
+//   const hideSpinner = (val) => {
+//     setShowSpinner(val);
+//     console.log("anjan");
+//   };
+
+//   return (
+//     <>
+//       {showSpinner ? (
+//         <div
+//           className="d-flex justify-content-center align-items-center"
+//           style={{ minHeight: "100vh" }}
+//         >
+//           <Spinner animation="border" variant="primary" />
+//         </div>
+//       ) : (
+//         <div>
+//           <JobbyHeader hideSpinner={hideSpinner} />
+//           <div className="notfound__container d-flex flex-column justify-content-center align-items-center">
+//             <img
+//               src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+//               alt="notfound"
+//               style={{ objectFit: "cover" }}
+//             />
+//             <div
+//               style={{ textAlign: "center", color: "white" }}
+//               className="p-4"
+//             >
+//               <h1>Page Not Found</h1>
+//               <p>We're sorry, the page you requested could not be found</p>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+import React, { Component } from "react";
 import JobbyHeader from "../JobbyHeader";
 import { Spinner } from "react-bootstrap";
 import "./index.css";
 
-export const NotFound = () => {
-  const [showSpinner, setShowSpinner] = useState(false);
-
-  const hideSpinner = (val) => {
-    setShowSpinner(val);
-    console.log("anjan");
+export class NotFound extends Component {
+  state = {
+    showSpinner: false,
   };
 
-  return (
-    <>
-      {showSpinner ? (
-        <div
-          className="d-flex justify-content-center align-items-center"
-          style={{ minHeight: "100vh" }}
-        >
-          <Spinner animation="border" variant="primary" />
-        </div>
-      ) : (
-        <div>
-          <JobbyHeader hideSpinner={hideSpinner} />
-          <div className="notfound__container d-flex flex-column justify-content-center align-items-center">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
-              alt="notfound"
-              style={{ objectFit: "cover" }}
-            />
-            <div
-              style={{ textAlign: "center", color: "white" }}
-              className="p-4"
-            >
-              <h1>Page Not Found</h1>
-              <p>We're sorry, the page you requested could not be found</p>
+  hideSpinner = (val) => {
+    this.setState({ showSpinner: val });
+  };
+
+  render() {
+    const { showSpinner } = this.state;
+    return (
+      <>
+        {showSpinner ? (
+          <div
+            className="d-flex justify-content-center align-items-center"
+            style={{ minHeight: "100vh" }}
+          >
+            <Spinner animation="border" variant="primary" />
+          </div>
+        ) : (
+          <div>
+            <JobbyHeader hideSpinner={this.hideSpinner} />
+            <div className="notfound__container d-flex flex-column justify-content-center align-items-center">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+                alt="notfound"
+                style={{ objectFit: "cover" }}
+              />
+              <div
+                style={{ textAlign: "center", color: "white" }}
+                className="p-4"
+              >
+                <h1>Page Not Found</h1>
+                <p>We're sorry, the page you requested could not be found</p>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </>
-  );
-};
+        )}
+      </>
+    );
+  }
+}
